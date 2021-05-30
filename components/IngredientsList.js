095b5dccac2f1444b2b9634866fa673175decf85
SadkiratSingh <sadkiratsingh@yahoo.com>
2021-05-30 14:50:09 +0530

    recipes improved

---
+++ components/IngredientsList.js
@@ -0,0 +1,13 @@
+// because we are using JSX
+import React from "react"
+import Ingredient from "./Ingredients"
+
+export default function IngredientsList({list}){
+    return(
+        <ul className = 'ingredients'>
+            {list.map((item,index)=>(
+                <Ingredient key={index} {...item} />
+            ))}
+        </ul>
+    )
+}
--- components/Recipe.js
+++ components/Recipe.js
@@ -1,23 +1,15 @@
 // because we are using JSX
 import React from "react"
+import IngredientsList from "./IngredientsList"
+import Instructions from "./Instructions"
 
 // export default is used when exporting single entity
 export default function Recipe({name,ingredients,steps}){
     return(
-        <section id='baked-salmon'>
+        <section id={name.toLowerCase().replace(/ /g)}>
             <h1>{name}</h1>
-            <ul className = 'ingredients'>
-                {ingredients.map((item,index)=>(
-                    <li key={index}>{item.name}</li>
-                ))}
-            </ul>
-            {/*create a seperate component*/}
-            <section className='instructions'>
-                <h2>Cooking Instructions</h2>
-                {steps.map((step,index)=>(
-                    <p key={index}>{step}</p>
-                ))}
-            </section>
+            <IngredientsList list={ingredients}/>
+            <Instructions title='Cooking Instructions' steps={steps} />
         </section>
     )
 }
