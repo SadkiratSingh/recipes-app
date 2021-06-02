a736aab38c5eaeac1b3420f216a883c8ec70d804
SadkiratSingh <sadkiratsingh@yahoo.com>
2021-06-02 15:37:21 +0530

    Star component complete

--- components/StarComponent.js
+++ components/StarComponent.js
@@ -1,16 +1,25 @@
 import React, {useState} from 'react';
 import {FaStar} from "react-icons/fa";
 
-const Star = ({selected = False}) =>(
-    <FaStar color = {selected ? "red" : "grey"} />
+const Star = ({selected = False,onselect= f=>f }) =>(
+    <FaStar color = {selected ? "red" : "grey"} onClick = {onselect}/>
 )
 
 export default function StarRating({totalStars = 5}){
     const createArray = (length) => [...Array(length)];
-    const [selectedStars] = useState(3);
+    const [selectedStars,setSelectedStars] = useState(3);
     return (
         <>
-        {createArray(totalStars).map((n,i)=><Star key={i} selected={selectedStars > i} />)}
+            {createArray(totalStars).map((n,i)=>
+                <Star 
+                    key={i} 
+                    selected={selectedStars > i} 
+                    onselect = {()=>setSelectedStars(i+1)}
+                />
+            )}
+            <p>
+                {selectedStars} of {totalStars}
+            </p>
         </>
     )
 }
