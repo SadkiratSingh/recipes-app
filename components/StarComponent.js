2f78dfaf1f0d2a734e0026b111d77d130ca859bb
SadkiratSingh <sadkiratsingh@yahoo.com>
2021-06-02 13:25:47 +0530

    using state variables

--- components/StarComponent.js
+++ components/StarComponent.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {FaStar} from "react-icons/fa";
+
+const Star = ({selected = False}) =>(
+    <FaStar color = {selected ? "red" : "grey"} />
+)
 
 export default function StarRating({totalStars = 5}){
     const createArray = (length) => [...Array(length)];
-    return createArray(totalStars).map((n,i)=><FaStar key={i} />)
+    const [selectedStars] = useState(3);
+    return (
+        <>
+        {createArray(totalStars).map((n,i)=><Star key={i} selected={selectedStars > i} />)}
+        </>
+    )
 }
